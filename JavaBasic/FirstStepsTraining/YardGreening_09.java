99031b4e5114e023135b6895fb522955bfb8af68
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2023-04-22 14:24:18 +0100

    Create YardGreening_09.java

---
+++ JavaBasic/FirstStepsTraining/YardGreening_09.java
@@ -0,0 +1,24 @@
+package FirstStepsTraining;
+
+import java.util.Scanner;
+
+public class YardGreening_09 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double area = Double.parseDouble(scanner.nextLine());
+
+        double m2Price = 7.61;
+        int discount = 18;
+
+        double costBeforeDiscount = area * m2Price;
+        double discountAmount = costBeforeDiscount * discount / 100;
+        double costAfterDiscount = costBeforeDiscount - discountAmount;
+
+        System.out.printf("The final price is: %f lv.%n", costAfterDiscount);
+        System.out.printf("The discount is: %f lv.", discountAmount);
+
+    }
+}
