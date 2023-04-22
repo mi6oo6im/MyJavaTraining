26807e24abdec51715ab2139144b3f75e7811186
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2023-04-22 14:24:16 +0100

    Create RectangleArea_03.java

---
+++ JavaBasic/FirstStepsTraining/RectangleArea_03.java
@@ -0,0 +1,13 @@
+package FirstStepsTraining;
+import java.util.Scanner;
+public class RectangleArea_03 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int a = Integer.parseInt(scanner.nextLine());
+        int b = Integer.parseInt(scanner.nextLine());
+        int result = a * b;
+        System.out.println(result);
+    }
+}
