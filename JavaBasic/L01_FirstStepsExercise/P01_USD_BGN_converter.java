b6e2c63baa583f478336a07cc1c6fbec5e365753
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2023-04-23 14:26:39 +0100

    Create P01_USD_BGN_converter.java

---
+++ JavaBasic/L01_FirstStepsExercise/P01_USD_BGN_converter.java
@@ -0,0 +1,17 @@
+package L01_FirstStepsExercise;
+
+import java.util.Scanner;
+
+public class P01_USD_BGN_converter {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double usd = Double.parseDouble(scanner.nextLine());
+        double bgn = usd * 1.79549;
+
+        System.out.println(bgn);
+
+    }
+}
