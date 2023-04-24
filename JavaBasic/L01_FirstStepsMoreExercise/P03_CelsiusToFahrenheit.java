b841a5e940b2a7540a7ccbcb36e884178cb7f72c
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2023-04-24 20:36:13 +0100

    Create P03_CelsiusToFahrenheit.java

---
+++ JavaBasic/L01_FirstStepsMoreExercise/P03_CelsiusToFahrenheit.java
@@ -0,0 +1,17 @@
+package L01_FirstStepsMoreExercise;
+
+import java.util.Scanner;
+
+public class P03_CelsiusToFahrenheit {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double celsius = Double.parseDouble(scanner.nextLine());
+
+        double fahrenheit = celsius * 1.8 + 32;
+
+        System.out.printf("%.2f", fahrenheit);
+    }
+}
