bbeb619fa9543e56329e4325f811d697ea6780ca
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2023-04-29 12:17:49 +0100

    Create P01_ExcellentResult.java

---
+++ JavaBasic/L02_ConditionalStatementsTraining/P01_ExcellentResult.java
@@ -0,0 +1,17 @@
+package L02_ConditionalStatements;
+
+import java.util.Scanner;
+
+public class P01_ExcellentResult {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+
+        if (number >= 5){
+            System.out.println("Excellent!");
+        }
+    }
+}
