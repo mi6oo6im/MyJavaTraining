91755a47208520addb1cf04fc95a6777b1fa5247
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2023-04-29 12:17:53 +0100

    Create P03_Even_Or_Odd.java

---
+++ JavaBasic/L02_ConditionalStatementsTraining/P03_Even_Or_Odd.java
@@ -0,0 +1,19 @@
+package L02_ConditionalStatements;
+
+import java.util.Scanner;
+
+public class P03_Even_Or_Odd {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+
+        if (number % 2 == 0){
+            System.out.println("even");
+        } else {
+            System.out.println("odd");
+        }
+    }
+}
