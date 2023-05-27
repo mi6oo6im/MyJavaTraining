1b363e76e0c519dfc3c7e69ab281b018ad9e3f39
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2023-05-27 09:34:59 +0100

    Create P06_MaxNumber.java

---
+++ JavaBasic/L07_WhileLoopTraining/P06_MaxNumber.java
@@ -0,0 +1,26 @@
+package L07_WhileLoopTraining;
+
+import java.util.Scanner;
+
+public class P06_MaxNumber {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int max = Integer.MIN_VALUE;
+
+        String input = scanner.nextLine();
+
+        while (!input.equals("Stop")) {
+            int number = Integer.parseInt(input);
+
+            if (number > max) {
+                max = number;
+            }
+
+            input = scanner.nextLine();
+        }
+        System.out.println(max);
+    }
+}
